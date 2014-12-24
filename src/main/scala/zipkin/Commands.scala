272cf4ee4db41f0d41c65f24a1ee21681a6a0b82
Lev Khomich <levkhomich@gmail.com>
2014-12-25 01:04:49 +0500

    fix zipkin.sh missing execution flag

--- src/main/scala/zipkin/Commands.scala
+++ src/main/scala/zipkin/Commands.scala
@@ -1,19 +1,27 @@
 package zipkin
 
+import java.io.File
+
 import scala.sys.process._
 
-object Install extends App {
-  "./project/zipkin.sh install".!
+trait Zipkin {
+  private[this] val script = new File("./project/zipkin.sh")
+
+  def zipkin(cmd: String): ProcessBuilder = {
+    // activator drops execution flag
+    if (!script.canExecute) {
+      if (script.setExecutable(true))
+        throw new RuntimeException("can not set executable flag on " + script.getPath)
+    }
+    stringSeqToProcess(script.getPath :: cmd :: Nil)
+  }
+
 }
 
-object Start extends App {
-  "./project/zipkin.sh start".run()
+object Start extends App with Zipkin {
+  zipkin("start").run()
 }
 
-object Stop extends App {
-  "./project/zipkin.sh stop".!
-}
-
-object Update extends App {
-  "./project/zipkin.sh update".!
+object Stop extends App with Zipkin {
+  zipkin("stop").!
 }
