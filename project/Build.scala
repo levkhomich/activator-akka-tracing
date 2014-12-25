a341cba1c098125f75a605a10f00b18b94dcaeb1
Lev Khomich <levkhomich@gmail.com>
2014-12-25 21:15:36 +0500

    bump akka-tracing version

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object ProjectBuild extends Build {
       Seq (
         name := "activator-akka-tracing",
         organization := "com.github.levkhomich",
-        version := "0.4-SNAPSHOT",
+        version := "0.4",
 
         homepage := Some(url("https://github.com/levkhomich/akka-tracing")),
         licenses := Seq("Apache Public License 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
@@ -25,8 +25,8 @@ object ProjectBuild extends Build {
         resolvers += "Maven Central Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
         libraryDependencies ++=
           Seq(
-            "com.github.levkhomich" %% "akka-tracing-core" % "0.4-SNAPSHOT" changing(),
-            "com.github.levkhomich" %% "akka-tracing-spray" % "0.4-SNAPSHOT" changing(),
+            "com.github.levkhomich" %% "akka-tracing-core" % "0.4",
+            "com.github.levkhomich" %% "akka-tracing-spray" % "0.4",
             "io.spray" %% "spray-can" % "1.3.2",
             "io.spray" %% "spray-client" % "1.3.2",
             "com.typesafe" % "config" % "1.2.1",
